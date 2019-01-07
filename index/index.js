8f3c20aafb67547f676d578934e4422e749bc979
Carl-ZB <1968565187@qq.com>
2019-01-07 19:31:03 +0800

    一个目录

---
+++ index/index.js
@@ -0,0 +1,16 @@
+const app = getApp()
+Page({
+  data: {
+    imgUrls: [
+      'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1545822735175&di=6059fdad4566343114adc23ebafd02fa&imgtype=0&src=http%3A%2F%2Ff.hiphotos.baidu.com%2Fimage%2Fpic%2Fitem%2Fb90e7bec54e736d16ad596ad96504fc2d4626954.jpg',
+      'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1545822735175&di=957b303f11d2a0d500a9f342b4f74603&imgtype=0&src=http%3A%2F%2Fc.hiphotos.baidu.com%2Fimage%2Fpic%2Fitem%2Ffd039245d688d43f95231451701ed21b0ff43b8e.jpg',
+      'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1545823735589&di=49beeaeb22a761908d5a5873dd2c2008&imgtype=0&src=http%3A%2F%2Fpic1.win4000.com%2Fwallpaper%2F2018-07-03%2F5b3b6458f4023.jpg'
+    ],
+    indicatorDots: true,
+    autoplay: true,
+    interval: 5000,
+    duration: 1000
+  },
+
+
+})
